e77bf7afc464d55e4e54d707f37fab9226ab40a4
Derlys Dominguez <derlys275@gmail.com>
2024-03-13 14:17:25 +0000

    feat: implement account-ui-transactions

--- demo/src/app/account/account-ui.component.ts
+++ demo/src/app/account/account-ui.component.ts
@@ -1,67 +1,16 @@
 import { CommonModule } from '@angular/common';
 import { Component, Input } from '@angular/core';
-import { Account } from './account-data-access.component';
+import { Account, Transaction } from './account-data-access.component';
 
 @Component({
   selector: 'dapp-account-ui',
   standalone: true,
   imports: [CommonModule],
-  template: ` <div>
+  template: `
+    <div>
       <h1 class="text-5xl font-bold cursor-pointer">Balance</h1>
     </div>
-
-    <div class="space-y-2">
-      <div class="flex justify-between">
-        <h2 class="text-2xl font-bold">Transaction History</h2>
-        <div class="space-x-2">
-          <span class="loading loading-spinner"></span>
-          <button class="btn btn-sm btn-outline">
-            <!--            <IconRefresh />-->
-          </button>
-        </div>
-      </div>
-      <pre class="alert alert-error">Error: </pre>
-
-      <div>
-        <div>No transactions found.</div>
-
-        <table
-          class="table border-4 rounded-lg border-separate border-base-300"
-        >
-          <thead>
-            <tr>
-              <th>Signature</th>
-              <th class="text-right">Slot</th>
-              <th>Block Time</th>
-              <th class="text-right">Status</th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr>
-              <th class="font-mono">
-                <a></a>
-              </th>
-              <td class="font-mono text-right">
-                <a></a>
-                <a></a>
-              </td>
-              <td></td>
-              <td class="text-right">
-                <div class="badge badge-error">Failed</div>
-
-                <div class="badge badge-success">Success</div>
-              </td>
-            </tr>
-
-            <tr>
-              <td colSpan="{4}" class="text-center">
-                <button class="btn btn-xs btn-outline"></button>
-              </td>
-            </tr>
-          </tbody>
-        </table>
-      </div>
-    </div>`,
+  `,
 })
 export class AccountUiComponent {}
 
@@ -149,4 +98,65 @@ export class AccountButtonsComponent {}
 })
 export class AccountTokensComponent {
   @Input() accounts: Account[] = [];
+}
+
+@Component({
+  selector: 'dapp-account-transactions',
+  standalone: true,
+  imports: [CommonModule],
+  template: ` <div class="space-y-2">
+    <div class="flex justify-between">
+      <h2 class="text-2xl font-bold">Transaction History</h2>
+      <div class="space-x-2">
+        <!--        <span class="loading loading-spinner"></span>-->
+        <button class="btn btn-sm btn-outline">
+          <!--            <IconRefresh />-->
+        </button>
+      </div>
+    </div>
+    <!--    <pre class="alert alert-error">Error: </pre>-->
+
+    <div>
+      <!--      <div>No transactions found.</div>-->
+
+      <table class="table border-4 rounded-lg border-separate border-base-300">
+        <thead>
+          <tr>
+            <th>Signature</th>
+            <th class="text-right">Slot</th>
+            <th>Block Time</th>
+            <th class="text-right">Status</th>
+          </tr>
+        </thead>
+        <tbody>
+          @for(transaction of transactions; track transaction){
+          <tr>
+            <th class="font-mono">
+              <a>{{ transaction.signature }}</a>
+            </th>
+            <td class="font-mono text-right">
+              <a>{{ transaction.slot }}</a>
+            </td>
+            <td>{{ transaction.blockTime }}</td>
+            <td class="text-right">
+              <!--              <div class="badge badge-error">Failed</div>-->
+
+              <div class="badge badge-success">{{ transaction.status }}</div>
+            </td>
+          </tr>
+
+          }
+        </tbody>
+        <tr>
+          <td colSpan="{4}" class="text-center">
+            <button class="btn btn-xs btn-outline">Show All</button>
+          </td>
+        </tr>
+      </table>
+    </div>
+  </div>`,
+  styles: ``,
+})
+export class AccountTransactionsComponent {
+  @Input() transactions: Transaction[] = [];
 }
