30b902efa6d6614bd49d3fbe710f11d2e5295603
Derlys Dominguez <derlys275@gmail.com>
2024-03-28 17:59:23 +0000

    feat: implement wallet-adapter

--- demo/src/app/app.config.ts
+++ demo/src/app/app.config.ts
@@ -1,7 +1,13 @@
 import { ApplicationConfig } from '@angular/core';
 import { provideRouter, withComponentInputBinding } from '@angular/router';
 import { appRoutes } from './app.routes';
+import { provideWalletAdapter } from '@heavy-duty/wallet-adapter';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(appRoutes, withComponentInputBinding())],
+  providers: [
+    provideRouter(appRoutes, withComponentInputBinding()),
+    provideWalletAdapter(undefined, { commitment: 'confirmed' }),
+    provideAnimationsAsync(),
+  ],
 };
